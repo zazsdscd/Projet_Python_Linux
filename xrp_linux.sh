93328ecc2e8af3836fe2d9f2d036cecca92fb484
Aurelien Bernhard <123908206+zazsdscd@users.noreply.github.com>
2023-03-26 21:24:15 +0200

    Create xrp_linux.sh

---
+++ xrp_linux.sh
@@ -0,0 +1,2 @@
+
+echo "$(date +%Y-%m-%d\ %H:%M:%S), $(curl -s 'https://coinmarketcap.com/fr/currencies/xrp/' | grep -oP '(?<=priceValue ).*(?=</div>)' | grep -Po '\d+\.\d+'| head -1 | cut -d ">" -f 2)" >> prix.csv
