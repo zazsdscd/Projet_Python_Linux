d1d3b7b80dff1d60f4ec2f32fbb5ad7fa7d138c1
Aurelien Bernhard <123908206+zazsdscd@users.noreply.github.com>
2023-03-27 14:04:51 +0200

    Update Projet-dashboard.py

--- Projet-dashboard.py
+++ Projet-dashboard.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 10 16:04:43 2023
-
-@author: aurel
-"""
 
 import dash
 from dash import dcc
@@ -13,14 +8,10 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 
 # Chargement des données
-df = pd.read_csv('home/ubuntu/prix.csv', sep=',', names=['Date', 'suppr', 'prix'])
-df.drop('suppr', axis=1, inplace=True)
-df['prix'] = df['prix'].astype(str)
-df['prix'] = '0.' + df['prix']
+df = pd.read_csv('/home/ubuntu/prices.csv', sep=',', names=['Date', 'prix'], na_values=[''])
 df['prix'] = df['prix'].astype(float)
 df['Date'] = pd.to_datetime(df['Date'])
 ts = pd.Series(df['prix'].values, index=df['Date'])
-df_daily = ts.resample('D').ohlc()
 
 # Calcul des statistiques
 volatility = round(ts.pct_change().std() * 100, 2)
@@ -33,8 +24,7 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
 
-    html.Div(children=[
-        html.Img(src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ajAJF0MnK35cgI51EuyAjWHiRYTTjKZgew&usqp=CAU',
+    html.Div(children=[html.Img(src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-ajAJF0MnK35cgI51EuyAjWHiRYTTjKZgew&usqp=CAU',
                  style={'width': '20%'}),
         html.H3(children='XRP Presentation',
                 style={'text-align': 'center', 'font-weight': 'bold', 'font-size': '45px', 'margin-left': '40px',
@@ -57,9 +47,7 @@ app.layout = html.Div(children=[
                 html.Tr(
                     children=[
                         html.Td(children='Volatility'),
-                        html.Td(children=f'{volatility}%',
-                                style={'color': 'red' if volatility > 10 else 'green'})
-                    ]
+                        html.Td(children=f'{volatility}%',style={'color': 'red' if volatility > 10 else 'green'})]
                 ),
                 html.Tr(
                     children=[
@@ -76,24 +64,23 @@ app.layout = html.Div(children=[
             ]
         ),
     ], style={'display': 'flex'}),
-  
-    
-  
-    
-    # Graphique de l'évolution des prix
+
     dcc.Graph(
-        id='xrp-prices',
+        id='xrp-price-chart',
         figure={
-            'data': [go.Scatter(x=df_daily.index, y=df_daily['close'], name='XRP Prices')],
-            'layout': go.Layout(
-                title='XRP Prices Evolution',
-                xaxis={'title': 'Date'},
-                yaxis={'title': 'Price (USD)'},
-                plot_bgcolor='white'
-            )
+            'data': [{
+                'x': df['Date'],
+                'y': df['prix'],
+                'type': 'line'
+            }], 'layout': {
+                'title': 'XRP Price over time',
+                'xaxis': {'title': 'Date'},
+                'yaxis': {'title': 'Price'}
+            }
         }
     )
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True,port=8050, host='localhost')
+        app.run_server(debug=True,port=8050, host='0.0.0.0')
+                                                               
